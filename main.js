1dc9bb0343780ce3abf682e744d8dfaad3833078
oas <oas@users.noreply.github.com>
2019-02-21 20:21:19 +0100

    we now adhere to the vue sfc specification

--- main.js
+++ main.js
@@ -13,41 +13,21 @@ module.exports = function (options) {
 	this.renderer = Renderer.createRenderer(options);
 
 	this.compile = function (path, context) {
-		// parse our custom .vue file that consists of two custom tags: <client> and <server>
-		const template = Compiler.parseComponent(FileSystem.readFileSync(path).toString());
-
-		let clientComponent;
-		let serverComponent;
-		// TODO: check if both custom tags exist... if not, react accordingly
-		for (const block of template.customBlocks) {
-			switch (block.type) {
-				case "client":
-					clientComponent = this.compileClientComponent(block);
-					break;
-				case "server":
-					serverComponent = this.compileServerComponent(path, block);
-					break;
-			}
-		}
-
-		const options = serverComponent;
-		options.template = clientComponent.template.content;
-
-		context.styles = "<style>" + clientComponent.styles[0].content.replace(/[\r\n\t]/g, "") + "</style>";
-		context.scripts = "<script>" + clientComponent.script.content + "</script>"; // TODO: use uglify-js to minify the content of <script>
-
-		return this.renderer.renderToString(new Vue(options), context);
-	};
-
-	this.compileClientComponent = function (block) {
-		// the client component is used for the http response:
+		// the template value is used for the http response:
 		// <template>: will be added as a vue template
 		// <style>: will be injected into the defined <style> tag of the website
 		// <script>: will be injected into the defined <script> tag of the website
-		return Compiler.parseComponent(block.content);
+		const component = this.compileComponent(FileSystem.readFileSync(path).toString(), path);
+		const descriptor = Compiler.parseComponent(component.template);
+
+		component.template = descriptor.template.content;
+		context.styles = "<style data-server-rendered=\"true\">" + descriptor.styles[0].content.replace(/[\r\n\t]/g, "") + "</style>";
+		context.scripts = "<script data-server-rendered=\"true\">" + descriptor.script.content + "</script>"; // TODO: use uglify-js to minify the content of <script>
+
+		return this.renderer.renderToString(new Vue(component), context);
 	};
 
-	this.compileServerComponent = function (path, block) {
+	this.compileComponent = function (content, path) {
 		// the server component contains the server logic and will be added to the vue instance
 		// the vue template defined in the <client> tag can react based on the server logic
 
@@ -56,7 +36,7 @@ module.exports = function (options) {
 		virtualModule.id = path;
 		virtualModule.filename = path;
 		virtualModule.paths = Module._nodeModulePaths(Path.dirname(path));
-		virtualModule._compile(Compiler.parseComponent(block.content).script.content, virtualModule.filename);
+		virtualModule._compile(content, virtualModule.filename);
 
 		return virtualModule.exports;
 	};
